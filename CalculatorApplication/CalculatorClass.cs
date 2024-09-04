25dec3f70fafcecbdadf0b3369f41cf2dc40cd89
EDF-Monke <delafuentesedriane@gmail.com>
2024-09-05 06:12:37 +0800

    Add files via upload

---
+++ CalculatorApplication/CalculatorClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+    internal class CalculatorClass
+    {
+        // Step 4 delegate
+        public delegate double Information(double arg1, double arg2);
+
+  
+        public event Information CalculateEvent
+        {
+            add
+            {
+                Console.WriteLine("Added the Delegate");
+               
+                _calculateEvent += value;
+            }
+            remove
+            {
+                Console.WriteLine("Removed the Delegate");
+               
+                _calculateEvent -= value;
+            }
+        }
+
+        private Information _calculateEvent;
+
+        // Step 6
+        public double GetSum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double GetDifference(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double GetProduct(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double GetQuotient(double num1, double num2)
+        {
+            
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero");
+            }
+            return num1 / num2;
+        }
+
+        // Step 12
+        public double PerformCalculation(double arg1, double arg2)
+        {
+            if (_calculateEvent != null)
+            {
+                return _calculateEvent(arg1, arg2);
+            }
+            throw new InvalidOperationException("No calculation method is set.");
+        }
+    }
+}
---
+++ CalculatorApplication/FrmCalculator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApplication
+{
+    public partial class FrmCalculator : Form
+    {
+        // Step 8 1/2
+        CalculatorClass cal;
+
+        public FrmCalculator()
+        {
+            InitializeComponent();
+            // Step 8 2/2
+            cal = new CalculatorClass();
+        }
+
+        // Property -> Event -> Click
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            double num1;
+            double num2;
+
+            try
+            {
+                //TextBoxes to double
+                num1 = Convert.ToDouble(txtBoxInput1.Text);
+                num2 = Convert.ToDouble(txtBoxInput2.Text);
+
+                double result = 0;
+                Func<double, double, double> calculationMethod = null;
+
+                //  calculation on selected operator
+                switch (cbOperator.SelectedItem.ToString())
+                {
+                    case "+":
+                        calculationMethod = cal.GetSum;
+                        break;
+
+                    case "-":
+                        calculationMethod = cal.GetDifference;
+                        break;
+
+                    case "*":
+                        calculationMethod = cal.GetProduct;
+                        break;
+
+                    case "/":
+                        calculationMethod = cal.GetQuotient;
+                        break;
+
+                    default:
+                        lblDisplayTotal.Text = "Select an operator";
+                        return;
+                }
+
+                try
+                {
+                    // delegate to  calculation
+                    result = calculationMethod(num1, num2);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    lblDisplayTotal.Text = ex.Message;
+                    return;
+                }
+
+                lblDisplayTotal.Text = result.ToString();
+            }
+            catch (FormatException)
+            {
+                lblDisplayTotal.Text = "Enter valid numbers";
+            }
+        }
+    }
+}
